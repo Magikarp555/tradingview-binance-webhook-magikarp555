e694b3b404472babb87b6a2eaa20dcf58357ac6d
Magikarp555 <108626362+Magikarp555@users.noreply.github.com>
2022-07-06 00:07:23 +0700

    adjust leverage command

--- src/models/command.ts
+++ src/models/command.ts
@@ -3,9 +3,9 @@ import { PositionSide } from 'binance'
 export interface Command {
   symbol: string
   side: PositionSide
-  leverage: number
   amountUSD: number
   setTp: boolean
   setSl: boolean
+  leverage: number
   onlyOneOrder: boolean
 }
